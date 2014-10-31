37321301c38700495c1eb81e0fff08f27713b10d
Pierre Lavaux <pierre.lvx@gmail.com>
2014-10-31 10:45:56 +0100

    trimmed trailing whitespace and empty lines

--- core/modules/Efass/Efass.php
+++ core/modules/Efass/Efass.php
@@ -1,32 +1,34 @@
 <?php
 
- 
+/**
+ * Class Efass
+ */
 class Efass extends CodonModule
 {
-	
+
 	public function index()
 	{
 		// Get all of our hubs, and list pilots by hub
 		$allhubs = OperationsData::GetAllHubs();
-		
+
 		if(!$allhubs) $allhubs = array();
-		
+
 		foreach($allhubs as $hub)
 		{
 			$this->set('title', $hub->name);
 			$this->set('icao', $hub->icao);
-			
+
 			$this->set('allpilots', PilotData::findPilots(array('p.hub'=>$hub->icao)));
-								
+
 			$this->render('pilots_list.tpl');
 		}
-		
+
 		$nohub = PilotData::findPilots(array('p.hub'=>''));
 		if(!$nohub)
 		{
 			return;
 		}
-		
+
 		$this->set('title', 'No Hub');
 		$this->set('icao', '');
 		$this->set('allpilots', $nohub);
@@ -46,21 +48,21 @@ class Efass extends CodonModule
 	}
 
 	protected function create_line_graph($title, $data)
-	{	
+	{
 		if(!$data)
 		{
 			$data = array();
 		}
-				
+
 		$bar_values = array();
 		$bar_titles = array();
 		foreach($data as $val)
 		{
-			
+
 			$bar_titles[] = $val->ym;
 			$bar_values[] = round(floatval($val->total));
 		}
-	
+
 		OFCharts::add_data_set($bar_titles, $bar_values);
 		echo OFCharts::create_area_graph($title);
 	}
